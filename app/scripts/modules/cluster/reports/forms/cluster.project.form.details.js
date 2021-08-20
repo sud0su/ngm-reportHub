53146b35dc46770390ed02c829625223ccb6003f
fakhrihawari <blitzfakhri@gmail.com>
2021-08-20 16:28:30 +0700

    bug-fix import and hide download when new project

--- app/scripts/modules/cluster/reports/forms/cluster.project.form.details.js
+++ app/scripts/modules/cluster/reports/forms/cluster.project.form.details.js
@@ -2796,15 +2796,24 @@ angular.module( 'ngm.widget.project.details', [ 'ngm.provider' ])
 					if (obj.implementing_partners) {
 						var impl_array = []
 						temp = obj.implementing_partners.split(',').map((x) => { return x.trim(); });
+						if ($scope.project.definition.implementing_partners && $scope.project.definition.implementing_partners.length){
+							angular.forEach(temp, function (e) {
 
-						angular.forEach(temp, function (e) {
+								selected_impl = $filter('filter')($scope.project.definition.implementing_partners, { organization: e });
 
-							selected_impl = $filter('filter')($scope.project.lists.organizations, { organization: e });
+								if (selected_impl.length) {
+									impl_array.push(selected_impl[0]);
+								}
+							})
+						}
+						// angular.forEach(temp, function (e) {
 
-							if (selected_impl.length) {
-								impl_array.push(selected_impl[0]);
-							}
-						})
+						// 	selected_impl = $filter('filter')($scope.project.lists.organizations, { organization: e });
+
+						// 	if (selected_impl.length) {
+						// 		impl_array.push(selected_impl[0]);
+						// 	}
+						// })
 						obj.implementing_partners = impl_array;
 					}
 
